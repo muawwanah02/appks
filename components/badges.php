f1b74177b7aa0cb98ae10d7158dbc945a013d333
muawwanah02 <94770371+muawwanah02@users.noreply.github.com>
2024-02-15 22:21:52 +0800

    Update badges.php

--- components/badges.php
+++ components/badges.php
@@ -1,5 +1,6 @@
 <?php
-function hitungQty($koneksi, $nama_tabel){
+function hitungQty($koneksi, $nama_tabel)
+{
     $select = "SELECT COUNT(*) qty FROM $nama_tabel";
     $result = mysqli_query($koneksi, $select);
     $row = mysqli_fetch_assoc($result);
@@ -7,14 +8,31 @@ function hitungQty($koneksi, $nama_tabel){
 }
 
 
+$pegawai_qty = hitungQty($koneksi, "pegawai");
 $orang_tua_qty = hitungQty($koneksi, "orang_tua");
 $anak_qty = hitungQty($koneksi, "anak");
-$pegawai_qty = hitungQty($koneksi, "pegawai");
 $desa_qty = hitungQty($koneksi, "desa");
-
+$gizi_anak_qty = hitungQty($koneksi, "gizi_anak");
+$kasus_stunting_qty = hitungQty($koneksi, "kasus_stunting");
 
 ?>
 <div id="badges" class="row g-3 my-2">
+    <?php
+    if ($_SESSION["level"] == "admin") {
+    ?>
+        <div class="col-md-6 col-lg-3">
+            <div class="p-3 bg-primary shadow-sm d-flex justify-content-around align-items-center rounded">
+                <div>
+                    <h3 class="fs-2"><?= $pegawai_qty ?></h3>
+                    <p class="fs-5 text-white">Pegawai</p>
+                </div>
+                <i class="fas fa-users fs-1 text-white bg-primary p-3"></i>
+            </div>
+        </div>
+    <?php
+    }
+    ?>
+
     <div class="col-md-6 col-lg-3">
         <div class="p-3 hijo-bg shadow-sm d-flex justify-content-around align-items-center rounded">
             <div>
@@ -36,22 +54,32 @@ $desa_qty = hitungQty($koneksi, "desa");
     </div>
 
     <div class="col-md-6 col-lg-3">
-        <div class="p-3 bg-primary shadow-sm d-flex justify-content-around align-items-center rounded">
+        <div class="p-3 bg-dark shadow-sm d-flex justify-content-around align-items-center rounded">
             <div>
-                <h3 class="fs-2"><?= $pegawai_qty ?></h3>
-                <p class="fs-5 text-white">Pegawai</p>
+                <h3 class="fs-2"><?= $desa_qty ?></h3>
+                <p class="fs-5 text-white">Desa</p>
             </div>
-            <i class="fas fa-users fs-1 text-white bg-primary p-3"></i>
+            <i class="fa-solid fa-city fs-1 text-white bg-dark p-3"></i>
+        </div>
+    </div>
+
+    <div class="col-md-6 col-lg-3">
+        <div class="p-3 bg-info shadow-sm d-flex justify-content-around align-items-center rounded">
+            <div>
+                <h3 class="fs-2"><?= $gizi_anak_qty ?></h3>
+                <p class="fs-5 text-white">Gizi Anak</p>
+            </div>
+            <i class="fa-solid fa-weight-scale fs-1 text-white bg-info p-3"></i>
         </div>
     </div>
 
     <div class="col-md-6 col-lg-3">
         <div class="p-3 bg-danger shadow-sm d-flex justify-content-around align-items-center rounded">
             <div>
-                <h3 class="fs-2"><?= $desa_qty ?></h3>
-                <p class="fs-5 text-white">Desa</p>
+                <h3 class="fs-2"><?= $kasus_stunting_qty ?></h3>
+                <p class="fs-5 text-white">Kasus Stunting</p>
             </div>
-            <i class="fa-solid fa-city fs-1 text-white bg-danger p-3"></i>
+            <i class="fa-solid fa-triangle-exclamation fs-1 text-white bg-danger p-3"></i>
         </div>
     </div>
 </div>
